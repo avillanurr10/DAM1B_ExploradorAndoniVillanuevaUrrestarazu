7c60e1b7cfa85344533b93de589aeea408cbf18e
Andoni <149407392+avillanurr10@users.noreply.github.com>
2025-01-04 11:41:36 +0100

    Update Enemigo.java

--- src/Clases/Enemigo.java
+++ src/Clases/Enemigo.java
@@ -1,62 +1,31 @@
-//3 de enero
+// 3 de enero con edicion 4
 package Clases;
-
-import java.util.Random;
-
+// Clase Enemigo que representa a los enemigos del juego
 public class Enemigo {
     private String nombre;
-    private Posicion posicionActual;
+    private Posicion posicion;
+    private Mapa mapa;
 
-    // Constantes para el movimiento
-    public static final int ARRIBA = 1;
-    public static final int ABAJO = 2;
-    public static final int DERECHA = 3;
-    public static final int IZQUIERDA = 4;
-
-    public Enemigo(String nombre) {
+    public Enemigo(String nombre, Mapa mapa) {
         this.nombre = nombre;
-        Random random = new Random();
-        int filaAleatoria = random.nextInt(6);
-        int columnaAleatoria = random.nextInt(20);
-        this.posicionActual = new Posicion(filaAleatoria, columnaAleatoria);
+        this.mapa = mapa;
+        this.posicion = mapa.inicializarEnemigo();
     }
 
-    public void moverseAleatorio(Mapa mapa) { // para diferenciar lo he llamado moverseAleatorio
-        Random random = new Random(); // se me hace mas cómodo realizar el random asi
-        int direccion = random.nextInt(4) + 1;
-
-        int nuevaFila = posicionActual.getCoordenadaFila();
-        int nuevaCol = posicionActual.getCoordenadaCol();
-
-        switch (direccion) {
-            case ARRIBA:
-                nuevaFila = Math.max(0, nuevaFila - 1);
-                break;
-            case ABAJO:
-                nuevaFila = Math.min(5, nuevaFila + 1);
-                break;
-            case DERECHA:
-                nuevaCol = Math.min(19, nuevaCol + 1);
-                break;
-            case IZQUIERDA:
-                nuevaCol = Math.max(0, nuevaCol - 1);
-                break;
-        }
-
-        if (mapa.obtenerElemento(nuevaFila, nuevaCol) == ' ') {
-            posicionActual.setCoordenadaFila(nuevaFila);
-            posicionActual.setCoordenadaCol(nuevaCol);
-        }
-    }
-
-    public Posicion getPosicionActual() {
-        return posicionActual;
+    public void mover() {
+        int direccion = new java.util.Random().nextInt(4) + 1;
+        mapa.moverEntidad(posicion, direccion, 'E');
     }
 
     public String getNombre() {
         return nombre;
     }
-    public void setPosicionActual(Posicion posicionActual) {
-        this.posicionActual = posicionActual;
+
+    public Posicion getPosicion() {
+        return posicion;
+    }
+
+    public void setPosicion(Posicion posicion) {
+        this.posicion = posicion;
     }
 }
